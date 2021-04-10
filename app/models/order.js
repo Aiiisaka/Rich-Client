14732c00e29f127b289737c1fca1c8db98368359
Maud LEFORT <21901572@etu.unicaen.fr>
2021-04-10 11:44:15 +0200

    Modification des models

--- app/models/order.js
+++ app/models/order.js
@@ -1,8 +1,18 @@
-import Model, { attr, hasMany } from '@ember-data/model';
+import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class OrderModel extends Model {
   @attr('date') dateCreation;
   @hasMany('orderdetail') orderdetail;
+  @attr('string') status;
+  @attr('number') amount;
+  @attr('number') toPay;
+  @attr('number') itemsNumber;
+  @attr('number') missingNumber;
+  @hasMany('orderdetail') details;
+  @belongsTo('timeSlot') idTimeSlot;
+  @belongsTo('employee') employee;
+  @belongsTo('user') user;
+
   get count() {
     return this.orderdetail.length;
   }
